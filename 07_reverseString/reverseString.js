b15d1b5443d2893498f70a64b2aeb2df52143f30
Paul <205002947+pcary1993@users.noreply.github.com>
2025-07-09 15:02:54 -0700

    Turn word into array reversed and then return as string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const array = word.split("");
+    array.reverse();
+    return array.join('');
 };
 
 // Do not edit below this line
